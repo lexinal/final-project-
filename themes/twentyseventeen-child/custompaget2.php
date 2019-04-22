395b97c1cf667d65a01a67866af13fa1f7e0f259
lexinal <46660807+lexinal@users.noreply.github.com>
2019-04-21 20:04:12 -0400

    Added Gif & Fixed Contact Form Appearance

--- themes/twentyseventeen-child/custompaget2.php
+++ themes/twentyseventeen-child/custompaget2.php
@@ -21,15 +21,16 @@
 			?>
 <br>
 
+<img src="https://media1.popsugar-assets.com/files/thumbor/2NiNvc6TAAKseQPH1TrDvv06ZTM/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2017/01/19/076/n/3019466/83db444b9172acf4_giphy/i/Every-time-something-big-happens-your-life-re-first-person-you-think-call.gif" alt="Dialing a Telephone GIF">
+
 <h1>Hello there!</h1>
 <p> To learn more about fitness opportunities or for any questions or concerns. Feel free to contact me using the contact form below. Looking forward to chatting! </p>
 
-<img src="https://media1.popsugar-assets.com/files/thumbor/2NiNvc6TAAKseQPH1TrDvv06ZTM/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2017/01/19/076/n/3019466/83db444b9172acf4_giphy/i/Every-time-something-big-happens-your-life-re-first-person-you-think-call.gif" alt="Dialing a Telephone GIF">
 
 <form action="mail.php" method="POST">
-<p>Name</p> <input type="text" name="name">
-<p>Email</p> <input type="text" name="email">
-<p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
+<br><p>Name</p> <input type="text" name="name">
+<br><p>Email</p> <input type="text" name="email">
+<br><p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
 <input type="submit" value="Send"><input type="reset" value="Clear">
 </form>
 
